4b0b58d629516fb810de6e1ec6f823fdcba975dd
wang xin <xin.wang3@thermofisher.com>
2019-04-29 14:46:03 +0800

    前台beta

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,17 +7,10 @@ Page({
     left: false ,
     right: false,
     activeIndex: 0,
-    photoPath: [
-      {
-        "photoname": "章泽天 17"
-      },
-      {
-        "photoname": "桥本环奈 16"
-      }
-    ],
-    tempFilePaths:'../../images/沈佳宜 18.png',
-    imgwidth:0, 
-    imgheight:0,   
+    photoPath: [{ username: "沈佳宜", photoname: "../../images/wx1 沈佳宜.png" },
+      { username: "章泽天", photoname: "../../images/wx2 章泽天.png" }
+      ],
+    name:''  
   },
   changeswiper: function(e) {
     var index = e.detail.current;
@@ -36,7 +29,7 @@ Page({
         left:false,
         right:false
       })
-    }, 1000);
+    }, 500);
   },
   //事件处理函数
   bindViewTap: function() {
@@ -55,24 +48,13 @@ Page({
       method: 'GET',
       dataType: 'json',
       responseType: 'text',
-      success: function(res) {
-        //console.log(res.data)
-      }
-    }),
-    
-    
-
-
-    wx.getImageInfo({
-      src: _this.data.tempFilePaths,
       success: function (res) {
         _this.setData({
-          imgwidth: res.path.split('/')[1].split(' ')[0],
-          imgheight: res.path.split('/')[1].split(' ')[1].split('.')[0]          
+          photoPath: res.data
         })
       }
     })
-  }  
+  }   
 
 
 })
